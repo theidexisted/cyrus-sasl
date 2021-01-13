f5a8aed49ee09615546526f93ec7c491390eaee3
ppenguin <invalid_ms_user@live.com>
2021-01-13 18:38:43 +0800

    Remove duplicated md5global header

--- include/md5global.h
+++ include/md5global.h
@@ -15,14 +15,14 @@ The following makes PROTOTYPES default to 0 if it has not already
 /* POINTER defines a generic pointer type */
 typedef unsigned char *POINTER;
 
-typedef signed char INT1;       /*  8 bits */
-typedef short INT2;         /* 16 bits */
-typedef int INT4;           /* 32 bits */
-/* There is no 64 bit type */
-typedef unsigned char UINT1;        /*  8 bits */
-typedef unsigned short UINT2;       /* 16 bits */
-typedef unsigned int UINT4;     /* 32 bits */
-/* There is no 64 bit type */
+typedef signed char INT1;		/*  8 bits */
+typedef short INT2;			/* 16 bits */
+typedef int INT4;			/* 32 bits */
+typedef long INT8;			/* 64 bits */
+typedef unsigned char UINT1;		/*  8 bits */
+typedef unsigned short UINT2;		/* 16 bits */
+typedef unsigned int UINT4;		/* 32 bits */
+typedef unsigned long UINT8;		/* 64 bits */
 
 /* PROTO_LIST is defined depending on how PROTOTYPES is defined above.
 If using PROTOTYPES, then PROTO_LIST returns the list, otherwise it
